29e01252c9ea0d41f11a43b970f045d6fb9b0d33
amine <radouaniamine02@gmail.com>
2024-01-24 02:18:10 +0100

    sir ya lms5ot

---
+++ contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" href="contact.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+<section id="contact">
+        <div class="contactdiv">
+            <h1>Contact:</h1>
+            <div class="contactdiv__form">
+                <form >
+                    <div>
+                        <label for="nom">Nom: </label><br>
+                        <input type="text" name="nom" id="nom"><br>
+                    </div>
+                    <div>
+                        <label for="prenom">Prenom: </label><br>
+                        <input type="text" name="prenom" id="prenom"><br>
+                    </div>
+                    <div>
+                        <label for="telephone">Telephone: </label><br>
+                        <input type="tel" name="telephone" id="telephone"><br>
+                    </div>
+                    <div>
+                        <label for="sujet">sujet: </label><br>
+                        <input type="text" name="sujet" id="sujet"><br>
+                    </div>
+                    <div>
+                        <label for="message">Message: </label><br>
+                        <textarea name="message" id="message" cols="30" rows="10"></textarea><br>
+                    </div>
+                    <div>
+                        <button type="submit" value="Submit">Submit</button>
+                        <input type="reset">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+  
